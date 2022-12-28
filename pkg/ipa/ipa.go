df1c27a83ad27b0bd40ec0a27feba1fed8282a58
Semyon Maryasin <simeon@maryasin.name>
2022-12-28 20:54:27 +0700

    more fix

--- pkg/ipa/ipa.go
+++ pkg/ipa/ipa.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"howett.net/plist"
 	"io"
+	"io/ioutil"
 	"path"
 )
 
@@ -32,7 +33,7 @@ func Info(ipaPath string) (info map[string]interface{}, err error) {
 		if rd, _err = file.Open(); _err != nil {
 			return nil, _err
 		}
-		data, _err := io.ReadAll(rd)
+		data, _err := ioutil.ReadAll(rd)
 		if _err != nil {
 			return nil, _err
 		}
